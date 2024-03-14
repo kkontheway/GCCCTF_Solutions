6f8ec7dfbaa19bf62e634f0f576682da5c95c541
kkontheway <2257077749@qq.com>
2024-03-14 23:47:03 +0800

    Make Template

---
+++ src/pincer/Minaminao.s.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import "forge-std/Script.sol";
+import "./pincer.sol";
+import "./Beyond.sol";
+import "./Vault.sol";
+import "./interfaces/IWETH.sol";
+
+contract Minaminao is Script {
+    Challenge challenge = Challenge(address(1));
+
+    function run() public {
+        vm.startBroadcast();
+        Vault vault = challenge.vault();
+        Beyond beyond = challenge.beyond();
+        IWETH weth = IWETH(challenge.WETH());
+        address tokenIn;
+        address tokenOut;
+        if (beyond.balanceOf(address(vault)) >= weth.balanceOf(address(vault))) {
+            tokenIn = address(beyond);
+            tokenOut = address(weth);
+        } else {
+            tokenIn = address(weth);
+            tokenOut = address(beyond);
+        }
+        uint256 amountIn = IERC20(tokenIn).balanceOf(address(vault));
+        require(vault.slippage() <= 5, "Non tolerable slippage");
+        vault.swap(amountIn, tokenIn, tokenOut);
+        vm.stopBroadcast();
+    }
+}
